932b0c6e02cdaf80d1e73c367ddd7d7384b30f30
Jared M. White <jwhite2292forward@gmail.com>
2023-04-21 15:49:15 -0500

    Add EXI headers directly to BrawlHeaders.

---
+++ rvl/Include/EXI/EXIUart.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <EXI/EXIBios.h>
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __OSEnableBarnacle(EXIChannel, u32);
+
+#ifdef __cplusplus
+}
+#endif
--- std/Include/types.h
+++ std/Include/types.h
@@ -1,8 +1,8 @@
-typedef unsigned long u64;
-typedef signed long s64;
+typedef unsigned long long u64;
+typedef signed long long s64;
 
-typedef unsigned int u32;
-typedef signed int s32;
+typedef unsigned long u32;
+typedef signed long s32;
 
 typedef unsigned short u16;
 typedef signed short s16;
@@ -12,3 +12,9 @@ typedef signed char s8;
 
 typedef float f32;
 typedef double f64;
+
+typedef int UNKWORD;
+typedef void UNKTYPE;
+
+enum { FALSE, TRUE };
+typedef int BOOL;
