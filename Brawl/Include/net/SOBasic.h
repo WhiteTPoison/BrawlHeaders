4a136f2e3ad3ff63e35f6efe1c39ed693b07e375
Jared M. White <jwhite2292forward@gmail.com>
2023-06-20 16:04:08 -0500

    Socket changes

--- Brawl/Include/net/SOBasic.h
+++ Brawl/Include/net/SOBasic.h
@@ -22,9 +22,47 @@ int SOClose(int socket);
 u32 SORecvFrom(int socket, void* buffer, u32 length, int flags, struct sockaddr* address, u32* address_len);
 u32 SOSendTo(int socket, const void* message, u32 length, int flags, const struct sockaddr* dest_addr, u32 dest_len);
 char* SOInetNtoA(struct in_addr in);
-
 u32 SOHtoNl(u32 hostlong);
 u32 SOHtoNs(u32 hostshort);
 u32 SOHtoHl(u32 netlong);
 u32 SOHtoHs(u32 netshort);
+u32 SONtoHs(short);
+
+enum {
+    IOCTL_SO_ACCEPT	= 1,
+    IOCTL_SO_BIND,
+    IOCTL_SO_CLOSE,
+    IOCTL_SO_CONNECT,
+    IOCTL_SO_FCNTL,
+    IOCTL_SO_GETPEERNAME, // todo
+    IOCTL_SO_GETSOCKNAME, // todo
+    IOCTL_SO_GETSOCKOPT,  // todo    8
+    IOCTL_SO_SETSOCKOPT,
+    IOCTL_SO_LISTEN,
+    IOCTL_SO_POLL,        // todo    b
+    IOCTLV_SO_RECVFROM,
+    IOCTLV_SO_SENDTO,
+    IOCTL_SO_SHUTDOWN,    // todo    e
+    IOCTL_SO_SOCKET,
+    IOCTL_SO_GETHOSTID,
+    IOCTL_SO_GETHOSTBYNAME,
+    IOCTL_SO_GETHOSTBYADDR,// todo
+    IOCTLV_SO_GETNAMEINFO, // todo   13
+    IOCTL_SO_UNK14,        // todo
+    IOCTL_SO_INETATON,     // todo
+    IOCTL_SO_INETPTON,     // todo
+    IOCTL_SO_INETNTOP,     // todo
+    IOCTLV_SO_GETADDRINFO, // todo
+    IOCTL_SO_SOCKATMARK,   // todo
+    IOCTLV_SO_UNK1A,       // todo
+    IOCTLV_SO_UNK1B,       // todo
+    IOCTLV_SO_GETINTERFACEOPT, // todo
+    IOCTLV_SO_SETINTERFACEOPT, // todo
+    IOCTL_SO_SETINTERFACE,     // todo
+    IOCTL_SO_STARTUP,           // 0x1f
+    IOCTL_SO_ICMPSOCKET =	0x30, // todo
+    IOCTLV_SO_ICMPPING,         // todo
+    IOCTL_SO_ICMPCANCEL,        // todo
+    IOCTL_SO_ICMPCLOSE          // todo
+};
 }
